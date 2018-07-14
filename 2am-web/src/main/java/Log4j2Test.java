0c96da58fdcbc3e791fe2295edd57c5ebd54d8cd
wuchaofan <2914416881@qq.com>
2018-07-14 22:48:56 +0800

    first commit

---
+++ 2am-web/src/main/java/Log4j2Test.java
@@ -0,0 +1,28 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4j2Test {
+
+    private final static Logger logger = LoggerFactory.getLogger(Log4j2Test.class);
+
+    public static void main(String[] args) {
+        long beginTime = System.currentTimeMillis();
+
+        for(int i = 0; i < 100000; i++) {
+            logger.trace("trace level");
+            logger.debug("debug level");
+            logger.info("info level");
+            logger.warn("warn level");
+            logger.error("error level");
+        }
+
+        try {
+            Thread.sleep(1000 * 61);
+        } catch (InterruptedException e) {}
+
+
+        logger.info("请求处理结束，耗时：{}毫秒", (System.currentTimeMillis() - beginTime));    //第一种用法
+        logger.info("请求处理结束，耗时：" + (System.currentTimeMillis() - beginTime)  + "毫秒");    //第二种用法
+
+    }
+}
